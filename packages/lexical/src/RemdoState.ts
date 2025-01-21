305e0e17d6ece7c98a931c44c827cd73a2699d78
Piotr Lewalski <piotr@lewalski.pl>
2025-01-21 16:23:33 +0000

    updating note focus

--- packages/lexical/src/RemdoState.ts
+++ packages/lexical/src/RemdoState.ts
@@ -22,7 +22,8 @@ export class RemdoState {
     return this._focusKey ? $getNodeByKey(this._focusKey) : undefined;
   }
 
-  setFocus(node: LexicalNode) {
-    this._focusKey = node.getKey() !== 'root' ? node.getKey() : undefined;
+  setFocus(node: LexicalNode | null) {
+    this._focusKey =
+      !node || node.getKey() === 'root' ? undefined : node.getKey();
   }
 }
